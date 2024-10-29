aeb261244179fd6d0190cb5a959bb90ee72031f7
Joshua <moisesj.rodriguez.ramirez0912@gmail.com>
2024-10-29 13:45:00 -0600

    icon update

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfziSucq4vaQkdXrQvaoNv_RSJDHO18vQ",
+  authDomain: "app-restaurante-80a86.firebaseapp.com",
+  projectId: "app-restaurante-80a86",
+  storageBucket: "app-restaurante-80a86.appspot.com",
+  messagingSenderId: "1043093078517",
+  appId: "1:1043093078517:web:02b9ba496413c83de820bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
